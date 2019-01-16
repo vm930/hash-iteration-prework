0e3cccff5616f8e75471fe186b3750d00cdbcb87
abstract-animation-9914 <vm930@hotmail.com>
2019-01-16 18:06:04 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,4 +8,11 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner =""
+  passengers.each {|suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end 
+  }
+  winner
 end
